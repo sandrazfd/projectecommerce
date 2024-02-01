e83300bd1ef5d183cae4105534620428ebc2415e
sandrazfd <sandrazfd@hotmail.com>
2024-02-01 16:25:47 -0300

    feat(layout): add header

---
+++ modules/feature/layout/src/lib/Header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'projectecommerce-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
