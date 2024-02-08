5e6140acd86c914941727865195c555a9ccadf9d
sandrazfd <sandrazfd@hotmail.com>
2024-02-07 21:11:17 -0300

    test:app component should contain header

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,20 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent,  RouterTestingModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent)
   });
 
+ it('should contain header',()=>{
+  const header:HTMLElement = fixture.nativeElement.querySelector('header')
+  expect(header).toBeTruthy();
+});
  
 
-  it(`should have as title 'projectecommerce'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('projectecommerce');
-  });
+ 
 });
