3178b2894d5086d3ce8293178fa55a64a4c5ed57
sandrazfd <sandrazfd@hotmail.com>
2024-03-07 21:06:00 -0300

    feat(product-search): add search to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from '@projectecommerce/layout';
+import { ProductSearchComponent } from '@projectecommerce/product-search';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    LayoutModule,
+    ProductSearchComponent,
+  ],
   selector: 'projectecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
