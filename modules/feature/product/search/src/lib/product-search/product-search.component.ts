7434ea13d4e5c8d08cbaa94d81d4a41e954f4e4b
sandrazfd <sandrazfd@hotmail.com>
2024-03-19 18:41:34 -0300

    feat(product-search):integrates api

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,17 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { mockProducts } from '@projectecommerce/product-data-access';
+import { ProductSearchService } from '@projectecommerce/product-data-access';
+import { Product } from 'modules/data-access/product/src/lib/models/product.model';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+} from 'rxjs';
 
 @Component({
   selector: 'projectecommerce-product-search',
   standalone: true,
   imports: [
     CommonModule,
-
     MatFormFieldModule,
     MatInputModule,
     MatAutocompleteModule,
@@ -20,7 +27,18 @@ import { mockProducts } from '@projectecommerce/product-data-access';
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
-export class ProductSearchComponent {
-  control = new FormControl('');
-  products = mockProducts;
+export class ProductSearchComponent implements OnInit {
+  control = new FormControl('', { nonNullable: true });
+  products$!: Observable<Product[]>;
+
+  constructor(private productSearchService: ProductSearchService) {}
+
+  ngOnInit() {
+    this.products$ = this.control.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter((text) => text.length > 1),
+      switchMap((text) => this.productSearchService.searchByName(text))
+    );
+  }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,20 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent,  RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(AppComponent)
+    fixture = TestBed.createComponent(AppComponent);
   });
 
- it('should contain header',()=>{
-  const header:HTMLElement = fixture.nativeElement.querySelector('header')
-  expect(header).toBeTruthy();
-});
- 
-
- 
+  it('should contain header', () => {
+    const header: HTMLElement = fixture.nativeElement.querySelector('header');
+    expect(header).toBeTruthy();
+  });
 });
