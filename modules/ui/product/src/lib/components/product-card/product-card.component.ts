b91d93af68208d57b2ed67cc57a681c122a9e249
sandrazfd <sandrazfd@hotmail.com>
2024-06-30 17:13:37 -0300

    feat(product-ui): add lib

---
+++ modules/ui/product/src/lib/components/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'projectecommerce-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {}
