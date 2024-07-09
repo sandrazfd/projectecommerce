6d538a3da41c37fa4504af40ac9a6c2024ad7e66
sandrazfd <sandrazfd@hotmail.com>
2024-07-09 20:30:23 -0300

    feat(product-detail): add cart integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from '@projectecommerce/layout';
+import { CartService } from '@projectecommerce/product-data-access';
 import { ProductSearchComponent } from '@projectecommerce/product-search';
+import { CartComponent } from '@projectecommerce/product-ui';
 
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 @Component({
   standalone: true,
   imports: [
@@ -11,11 +13,13 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     LayoutModule,
     ProductSearchComponent,
     MatSnackBarModule,
+    CartComponent,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'projectecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'projectecommerce';
+  quantity = inject(CartService).quantity;
 }
